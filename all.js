4d5437c2f5c9b2e94e946779c757967894ec5ae8
eason112 <126652410+eason112@users.noreply.github.com>
2024-12-04 11:22:42 +0800

    Add files via upload

---
+++ all.js
@@ -0,0 +1,114 @@
+let loadedImages = 0;
+
+function isMobileDevice() {
+  const userAgent = navigator.userAgent.toLowerCase();
+  return /iphone|ipod|android|windows phone|blackberry|iemobile/.test(userAgent);
+}
+
+document.getElementById('exit-btn').addEventListener('click', function() {
+    //loadedImages = imagesToPreload.length;
+    loadGame2();
+  });
+  document.getElementById('jumpBtn').addEventListener('click', function() {
+    console.log(this.textContent)
+    if(this.textContent==='對話'){
+      showDialog("你好，玩家！一起去挖蛤蠣吧！");
+      //loadGame1();
+    }
+    
+    //preloadImages();
+  });
+  // 初始化遊戲2的邏輯
+  function loadGame2() {
+    console.log("已經切換到巧虎遊戲！");
+    document.getElementById('game1').style.display = 'none';
+    document.getElementById('game2').style.display = 'block';
+    
+    // 啟用遊戲2的樣式和JavaScript
+    //document.getElementById('game1-stylesheet').disabled = true;
+    //document.getElementById('game2-stylesheet').disabled = false;
+    //document.getElementById('game1-script').disabled = true;
+    const script = document.getElementById('game2-script');
+    script.src = '網頁/game.js';  // 設定腳本來源
+  }
+  function loadGame1() {
+    console.log("已經切換到巧虎遊戲！");
+    document.getElementById('game1').style.display = 'block';
+    document.getElementById('game2').style.display = 'none';
+
+    const script1 = document.getElementById('game1-script1');
+    script1.src = '小遊戲/new/game.js';
+    const script2 = document.getElementById('game1-script2');
+    script2.src = '小遊戲/new/script.js';
+    const script3 = document.getElementById('game1-script3');
+    script3.src = '小遊戲/new/sound.js';
+    const startButton = document.getElementById('start-btn');
+    const rect = startButton.getBoundingClientRect();
+    console.log(rect.x,rect.y)
+    console.log(rect.width,rect.height)
+    showGuideArrow(rect.left+rect.width-rect.width/6,rect.top+rect.height-rect.height/2,0);
+    //loadgame();
+    
+    // 啟用遊戲2的樣式和JavaScript
+    //document.getElementById('game1-stylesheet').disabled = false;
+    //document.getElementById('game1-script').disabled = true;
+  }
+const imagesToPreload = [
+    '/小遊戲/new/images/image1.png',
+    '/小遊戲/new/images/image2.png',
+    '/小遊戲/new/images/image3.png',
+    '/小遊戲/new/images/image4.png',
+    '/小遊戲/new/images/image5.png',
+    '/小遊戲/new/images/up0.png',
+    '/小遊戲/new/images/up1.png',
+    '/小遊戲/new/images/up2.png',
+    '/小遊戲/new/images/up3.png',
+];
+
+
+
+function preloadImages() {
+    imagesToPreload.forEach((src) => {
+        const img = new Image();
+        img.src = src;
+        img.onload = () => {
+           // console.log(img.src);
+            loadedImages++;
+            // 当所有图片加载完毕后，可以开始渲染或启动动画
+            if (loadedImages === imagesToPreload.length) {
+                //loadGame1();
+                hideLoadingScreen();
+            }
+        };
+    });
+}
+
+const dialogBox = document.getElementById('dialogBox');
+const dialogText = document.getElementById('dialogText');
+const receiveDialogBtn = document.getElementById('receiveDialogBtn');
+
+receiveDialogBtn.addEventListener('click', function() {
+  hideDialog();  // 點擊後隱藏對話框
+  loadGame1();
+});
+
+document.getElementById("dialogBox").addEventListener("click", function(e) {
+  // 確保點擊的是對話框的背景區域，而不是內容
+  hideDialog();
+  
+});
+
+function showDialog(text) {
+  dialogBox.style.display = 'block';  // 顯示對話框
+  dialogText.textContent = text;      // 更新對話框內容
+}
+
+// 隱藏對話框的函數
+function hideDialog() {
+  dialogBox.style.display = 'none';   // 隱藏對話框
+}
+
+function hideLoadingScreen() {
+  document.getElementById('loadingScreen').style.display = 'none';
+  //document.getElementById('gameScreen').style.display = 'block';
+}
---
+++ 小遊戲/new/script.js
@@ -0,0 +1,113 @@
+window.onload = function() {
+  loadgame();
+  preloadImages();
+};
+
+function loadgame(){
+
+  const bgm = document.getElementById('bgm');
+  const bgm2 = document.getElementById('bgm2');
+  //var t = window.devicePixelRatio;
+ // document.body.style.zoom=1/t;
+    const startButton = document.getElementById('start-btn');
+    const gameInstructions = document.getElementById('game-info');
+    const background = document.querySelector('.background');
+    const beachArea = document.querySelector('.beach-area');
+    const rect = startButton.getBoundingClientRect();
+    //showGuideArrow(parseInt(startButton.style.left,10),parseInt(startButton.style.top,10));
+    showGuideArrow(rect.left,rect.top+30,0);
+    // 遊戲開始邏輯
+    //startButton.removeEventListener('click',starthandle);
+    startButton.addEventListener('touchstart', starthandle);
+    startButton.addEventListener('click', starthandle);
+    function starthandle() {
+      // 隱藏遊戲說明介面
+      //console.log("123546")
+      startButton.classList.add('active');  // 加入active類
+      playSound('clickbutton');
+      playBGM(bgm);
+      document.getElementById('game-info').style.display = 'none';
+      document.getElementById('difficulty-selection').style.display = 'block';
+      hideGuideArrow();
+      const rect = document.getElementById('easy-btn').getBoundingClientRect();
+      showGuideArrow(rect.left+rect.width-rect.width/6,rect.top+rect.height-rect.height/3,0);
+      
+      // 顯示遊戲區域
+      
+    }
+    //document.getElementById('easy-btn').removeEventListener('click', easyhandle);
+    if(isMobileDevice()){
+      document.getElementById('easy-btn').addEventListener('touchstart', easyhandle);
+    }
+    else{
+      document.getElementById('easy-btn').addEventListener('click', easyhandle);
+    }
+    function easyhandle() {
+        playSound('clickbutton');
+        document.getElementById('difficulty-selection').style.display = 'none';
+        background.style.display = 'block';
+        hideGuideArrow();
+  
+      // 在這裡開始倒數計時（從 time.js 引入倒數計時邏輯）
+        startCountdown(); // 設定為簡單模式
+    }
+    document.getElementById('easy-btn').addEventListener('touchend', function(){
+      this.classList.remove('active');
+    });
+}
+
+function startCountdown() {
+    const timerElement = document.getElementById('timer');
+    let timeLeft = 60;
+    timerElement.textContent = timeLeft;
+    const countdownDisplay = document.getElementById('countdown-display');
+    let countdownTime = 2;  // 設定倒數時間為3秒
+    countdownDisplay.textContent = 3;
+    countdownDisplay.style.display = 'block';  // 顯示倒數顯示區域
+  
+    // 使用 setInterval 來控制倒數
+    playSound('countdown');
+    const countdownInterval = setInterval(function() {
+      countdownDisplay.textContent = countdownTime;  // 顯示當前倒數數字
+      countdownTime--;  // 倒數減1
+  
+      // 當倒數結束後顯示 START! 並開始遊戲
+      if (countdownTime < 0) {
+        clearInterval(countdownInterval);  // 停止倒數
+        countdownDisplay.textContent = 'START!';  // 顯示「START!」
+        setTimeout(function() {
+            countdownDisplay.style.display = 'none';  // 隱藏倒數顯示區域
+            startGame();  // 開始遊戲
+          }, 1000);  // 1秒後開始遊戲
+          // 顯示 1 秒後開始遊戲
+      }
+    }, 1000);  // 每秒更新一次倒數顯示
+  }
+
+  function showGuideArrow(x,y,move) {
+    const guideArrow = document.getElementById('guide-arrow');
+        // 轉換 px 為百分比
+    const containerWidth = window.innerWidth; // 使用視窗寬度作為容器寬度
+    const containerHeight = window.innerHeight; // 使用視窗高度作為容器高度
+      
+    // 計算百分比
+    const xPercentage = (x / containerWidth) * 100;  // 將px轉為百分比
+    const yPercentage = (y / containerHeight) * 100;  // 將px轉為百分比
+    console.log(xPercentage,yPercentage);
+    guideArrow.style.display = 'block';  // 顯示指引箭頭
+    guideArrow.style.left = x + 'px';  // 設定箭頭的水平位置
+    guideArrow.style.top = y + 'px';   // 設定箭頭的垂直位置
+    guideArrow.style.left = xPercentage + '%';  // 設定箭頭的水平位置
+    guideArrow.style.top = yPercentage + '%';   // 設定箭頭的垂直位置
+
+    guideArrow.style.animation='pulse 1.5s ease-in-out infinite';
+    if(move)
+    {
+      guideArrow.style.animation='arrowmove 4s linear infinite';
+    }
+
+  }
+  function hideGuideArrow() {
+    const guideArrow = document.getElementById('guide-arrow');
+    guideArrow.style.display = 'none';  // 顯示指引箭頭
+  }
