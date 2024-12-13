cba542ca93cccce33baa9df24e8cd627f6a20158
eason112 <126652410+eason112@users.noreply.github.com>
2024-12-13 11:59:35 +0800

    Add files via upload

--- all.js
+++ all.js
@@ -11,45 +11,23 @@ window.onload = function() {
 
   // 初始化遊戲2的邏輯
 function loadGame2() {
-    console.log("已經切換到巧虎遊戲！");
+    console.log("已經切換到主畫面！");
     document.getElementById('login').style.display = 'none';
     document.getElementById('game1').style.display = 'none';
     document.getElementById('game2').style.display = 'block';
     startGame();
-    //document.getElementById('game2').classList.add('visible');
-    //document.getElementById('game1').classList.remove('visible');
-    
-    //unloadGame1Scripts();
-    // 啟用遊戲2的樣式和JavaScript
-    //document.getElementById('game1-stylesheet').disabled = true;
-    //document.getElementById('game2-stylesheet').disabled = false;
-    //document.getElementById('game1-script').disabled = true;
-    //const script = document.getElementById('game2-script');
-    //script.src = 'major/game.js';  // 設定腳本來源
-  }
+}
+
 function loadGame1() {
-    console.log("已經切換到巧虎遊戲！");
+    console.log("已經切換到小遊戲！");
     document.getElementById('game1').style.display = 'block';
     document.getElementById('game2').style.display = 'none';
+    playBGM(bgm);
     stopGame();
-    //document.getElementById('game1').classList.add('visible');
-    //document.getElementById('game2').classList.remove('visible');
-    //loadGame1Scripts();
+}
 
-    //const script1 = document.getElementById('game1-script1');
-    //script1.src = 'littlegame/new/game.js';
-    //const script2 = document.getElementById('game1-script2');
-    //script2.src = 'littlegame/new/script.js';
-    //const script3 = document.getElementById('game1-script3');
-    //script3.src = 'littlegame/new/sound.js';
-    //loadgame();
-    
-    // 啟用遊戲2的樣式和JavaScript
-    //document.getElementById('game1-stylesheet').disabled = false;
-    //document.getElementById('game1-script').disabled = true;
-  }
-  function loadlogin() {
-    console.log("已經切換到巧虎遊戲！");
+function loadlogin() {
+    console.log("已經切換到登入畫面！");
     document.getElementById('game1').style.display = 'none';
     document.getElementById('game2').style.display = 'none';
     document.getElementById('login').style.display = 'block';
@@ -57,7 +35,7 @@ function loadGame1() {
 
 }
 
-    // 動態載入遊戲1的所有JavaScript
+// 動態載入遊戲1的所有JavaScript
 function loadGame1Scripts() {
   // 如果遊戲1的腳本尚未加載，則加載它們
   if (!document.getElementById('game1-script1')) {
@@ -75,7 +53,7 @@ function loadGame1Scripts() {
   }
 }
 
-    // 卸載遊戲1的JavaScript
+// 卸載遊戲1的JavaScript
 function unloadGame1Scripts() {
   // 移除遊戲1的 JavaScript
   const script1 = document.getElementById('game1-script1');
@@ -118,28 +96,20 @@ function preloadImages() {
             if (loadedImages === imagesToPreload.length) {
                 //loadGame1();
                 //loadCSS();
-                
-                
                 hideLoadingScreen();
             }
         };
     });
 }
 
-
-
-
 function showLoadingScreen() {
   document.getElementById('loadingScreen').style.display = 'block';
-  
-  //document.getElementById('gameScreen').style.display = 'block';
 }
 
 function hideLoadingScreen() {
   document.getElementById('loadingScreen').style.display = 'none';
-  
-  //document.getElementById('gameScreen').style.display = 'block';
 }
+
 function isMobileDevice() {
   const userAgent = navigator.userAgent.toLowerCase();
   return /iphone|ipod|android|windows phone|blackberry|iemobile/.test(userAgent);
